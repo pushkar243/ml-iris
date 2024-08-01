284ab5ca11537ced90e489c973e6731d178139b6
Pushkar Verma <pushkar.verma@hotmail.com>
2024-08-02 02:41:05 +0530

    added test file

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,19 +2,18 @@ import pytest
 import joblib
 import pandas as pd
 
-
 def test_model_load():
     model = joblib.load('model/model.pkl')
     assert model is not None
 
-
 def test_model_prediction():
     model = joblib.load('model/model.pkl')
-    data = pd.DataFrame([[5.1, 3.5, 1.4, 0.2]], 
-                        columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+    data = pd.DataFrame(
+        [[5.1, 3.5, 1.4, 0.2]],
+        columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    )
     prediction = model.predict(data)
     assert prediction[0] in ['setosa', 'versicolor', 'virginica']
-
 
 if __name__ == '__main__':
     pytest.main()
