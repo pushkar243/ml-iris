f64b6c8dd8041e22ef80d4df41e3728702e8ffaf
Pushkar Verma <pushkar.verma@hotmail.com>
2024-08-02 02:31:31 +0530

    fixed error due to lint in model and train1

--- model/train.py
+++ model/train.py
@@ -12,7 +12,8 @@ y = data['species']
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.2, random_state=42)
+ test_size=0.2, random_state=42
+ )
 
 # Train model
 model = RandomForestClassifier()
