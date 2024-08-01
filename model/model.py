aa591edeec3b97ec1def1b10b2ec9213d2c4a3c9
Pushkar Verma <pushkar.verma@hotmail.com>
2024-08-02 02:17:47 +0530

    change 3

--- model/model.py
+++ model/model.py
@@ -3,19 +3,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
+
 # Load dataset
 data = pd.read_csv('../data/iris.csv')
+
 
 # Preprocess data
 X = data.drop('species', axis=1)
 y = data['species']
 
+
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 # Train model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
+
 
 # Save model
 joblib.dump(model, 'model.pkl')
