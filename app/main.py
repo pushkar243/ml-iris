930a28d7dc48cee826143dcb4141cee2644a0b09
Pushkar Verma <pushkar.verma@hotmail.com>
2024-08-02 01:56:33 +0530

    my files

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load model
+model = joblib.load('model/model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    data_df = pd.DataFrame(data, index=[0])
+    prediction = model.predict(data_df)
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
