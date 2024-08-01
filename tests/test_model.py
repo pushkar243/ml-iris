77cdac447bc9cd0a1443961105d9277a06d57754
Pushkar Verma <pushkar.verma@hotmail.com>
2024-08-02 03:18:33 +0530

    renamed file2

--- tests/test_model.py
+++ tests/test_model.py
@@ -12,7 +12,7 @@ def test_model_prediction():
     model = joblib.load('model.pkl')
     data = pd.DataFrame(
         [[5.1, 3.5, 1.4, 0.2]],
-        columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+        columns=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
     )
     prediction = model.predict(data)
     assert prediction[0] in ['setosa', 'versicolor', 'virginica']
